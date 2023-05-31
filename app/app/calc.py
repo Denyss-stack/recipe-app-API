714d14cb86c0423f2ace5293209c37f0f97ba7ce
ww2 programmer <denyssenchuk2005@gmail.com>
2023-05-31 11:29:44 +0300

    Another temporary test

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,6 +2,11 @@
 Calc function
 """
 
-def add(x,y):
-    """Adding x , y and return result."""
+
+def add(x, y):
+    """Adding x, y and return result. """
     return x + y
+
+
+def subtract(x, y):
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -11,6 +11,13 @@ class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
         """Test adding together"""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
+
+
+    def test_subtract_nubmers(self):
+        """Test subtact numbers"""
+        res = calc.subtract(10,15)
+
+        self.assertEqual(res, 5)
