fe0966d6979d4a818ba946f0f648b4641e147a41
ww2 programmer <denyssenchuk2005@gmail.com>
2023-06-03 20:50:12 +0300

    Configure test for wait_db_command

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,12 @@
+"""
+Django command for wait the database is available
+"""
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """
+    Django command for wait the database
+    """
+
+    def handle(self, *args, **options):
+        pass
