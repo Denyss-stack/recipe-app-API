1f6e1ea50b8fd47400ccfdbbf5bd401c93d126bb
ww2 programmer <denyssenchuk2005@gmail.com>
2023-06-04 14:03:39 +0300

    Wait_for_db configure and lint

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -18,6 +18,6 @@ class Command(BaseCommand):
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
-                self.stdout.write('Database is anavailable, wait for 1 second') 
+                self.stdout.write('Database is anavailable, wait for 1 second') # noqa 
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database is available!'))
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -13,7 +13,7 @@ class CommandTests(SimpleTestCase):
     """Test command"""
     def test_wait_for_db_ready(self, patched_check):
         """Tests waiting for database if database ready"""
-        patched_check.return_value = True 
+        patched_check.return_value = True # noqa 
         call_command('wait_for_db')
         patched_check.assert_called_once_with(databases=['default']) # noqa 
 
@@ -21,7 +21,7 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting Operational error"""
         patched_check.side_effect = [Psycopg2OpError] * 2 + \
-            [OperationalError] * 3 + [True] 
+            [OperationalError] * 3 + [True] # noqa 
         # The first two times we call the mocked method. \
         # We want it to raise the pcycopg2 error, \ # noqa 
         # so it raises the error.
