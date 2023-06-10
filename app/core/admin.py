d4ecfdd047ab882033713b16719f464f035214ce
ww2 programmer <denyssenchuk2005@gmail.com>
2023-06-10 15:14:31 +0300

    Implemented code in admin.py (**test pass**)

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,1 +1,16 @@
-from django.contrib import admin # noqa
+"""
+Djnago admin customization
+"""
+from django.contrib import admin  # noqa
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define admin pages for uses"""
+
+    ordering = ["id"]
+    list_display = ["email", "name"]
+
+
+admin.site.register(User, UserAdmin)
