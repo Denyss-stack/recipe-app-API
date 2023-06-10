f53e3a2dc27ab0034df5dc009c1b288666e33917
ww2 programmer <denyssenchuk2005@gmail.com>
2023-06-10 15:51:13 +0300

    Adding fildsets and configure the admin site

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,6 +4,7 @@ Djnago admin customization
 from django.contrib import admin  # noqa
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class UserAdmin(BaseUserAdmin):
@@ -11,6 +12,21 @@ class UserAdmin(BaseUserAdmin):
 
     ordering = ["id"]
     list_display = ["email", "name"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+    readonly_fields = ["last_login"]
 
 
 admin.site.register(User, UserAdmin)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -30,3 +30,10 @@ class AdminTest(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        url = reverse(
+            "admin:core_user_change", args=[self.user.id]
+        )  # /admin/core/user/1/change/ # noqa
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
