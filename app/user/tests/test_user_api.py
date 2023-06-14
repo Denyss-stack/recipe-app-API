d1162a4e0ac8c2b63424576ab71d316b8336d27d
ww2 programmer <denyssenchuk2005@gmail.com>
2023-06-14 14:01:00 +0300

    Completed fixing lint errors

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -20,7 +20,7 @@ class PublicUserAPITesta(TestCase):
     """"Test the public features of the user API."""
 
     def setUp(self):
-      self.client = APIClient()
+        self.client = APIClient()
 
     def test_create_user_success(self):
         """Test creating a user is successful."""
@@ -32,7 +32,7 @@ class PublicUserAPITesta(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        user = get_user_model().objects.get(email = payload["email"])
+        user = get_user_model().objects.get(email=payload["email"])
         self.assertTrue(user.check_password(payload["password"]))
 
         response_data = res.data.copy()
@@ -58,7 +58,7 @@ class PublicUserAPITesta(TestCase):
     def test_user_passwort_too_short(self):
         """Test an error is returned if password is unavailable."""
         payload = {
-              "email":' test@example.com',
+              "email": 'test@example.com',
               "password": "",
               "name": "Test name",
         }
@@ -66,6 +66,6 @@ class PublicUserAPITesta(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
-          email = payload["email"]
+          email=payload["email"]
         ).exists()
         self.assertFalse(user_exists)
