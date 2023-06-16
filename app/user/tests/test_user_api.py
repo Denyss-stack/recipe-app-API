866fb8df4ad44e1ffa2ea193f29566c07271a44d
ww2 programmer <denyssenchuk2005@gmail.com>
2023-06-16 20:39:28 +0300

    Testing token completed(not implemented token API)

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -84,3 +84,24 @@ class PublicUserAPITesta(TestCase):
         res = self.client.post(TOKEM_URL, payload)
         self.assertIn("token", res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_bad_credentials(self):
+        """Test returns error when credentals invalid"""
+        create_user(email="test@example.com", password="goodpassword")
+        payload = {
+            "email": "test@example.com",
+            "password": "badpassword",
+        }
+        res = self.client.post(TOKEM_URL, payload)
+        self.assertNotIn("token", res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_blank_password(self):
+        """Test returns error when password is unavailable"""
+        payload = {
+            "email": "test@example.com",
+            "password": "",
+        }
+        res = self.client.post(TOKEM_URL, payload)
+        self.assertNotIn("token", res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
