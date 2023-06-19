59b330ed1f66c172fdca6a40fa74bbef5083fcbf
ww2 programmer <denyssenchuk2005@gmail.com>
2023-06-19 21:11:07 +0300

    Test for create recipe (Not implemented)

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,10 @@
 """
 Test for models
 """
+from decimal import Decimal
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
 
 
 class ModelTest(TestCase):
@@ -41,7 +43,20 @@ class ModelTest(TestCase):
         user = get_user_model().objects.create_superuser(
             "test@example.com",
             "test123",
-            )
+        )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe_successful(self):
+        """Test creating recipe is successful"""
+        user = get_user_model().objects.create_user(
+            "test@example.com", "testpass123"
+        )  # noqa
+        recipe = models.Recipe.objects.create(
+            user=user,
+            time_minutes=5,
+            price=Decimal("5.50"),
+            description="Sample recipe description",
+        )
+        self.assertEqual(str(recipe), recipe.title)
