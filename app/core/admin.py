4f1a0c73351ebb877b564a9b4b644df9f7d0f67f
ww2 programmer <denyssenchuk2005@gmail.com>
2023-06-20 22:33:53 +0300

    Implement Recipe model

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,7 +3,7 @@ Djnago admin customization
 """
 from django.contrib import admin  # noqa
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+from .models import User, Recipe
 from django.utils.translation import gettext_lazy as _
 
 
@@ -47,3 +47,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Recipe)
