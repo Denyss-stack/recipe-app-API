f016d45070a00081191f75ed5565200fc3665259
ww2 programmer <denyssenchuk2005@gmail.com>
2023-06-28 23:01:39 +0300

    Implement tag model

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,7 +3,7 @@ Djnago admin customization
 """
 from django.contrib import admin  # noqa
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User, Recipe
+from .models import User, Recipe, Tag
 from django.utils.translation import gettext_lazy as _
 
 
@@ -48,3 +48,4 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Recipe)
+admin.site.register(Tag)
