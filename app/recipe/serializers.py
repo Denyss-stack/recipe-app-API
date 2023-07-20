cc7fc19cd3f3e6e09b88e9f66fc637729f253433
ww2 programmer <denyssenchuk2005@gmail.com>
2023-07-20 17:36:43 +0300

    Implement creating ingreduent feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -31,10 +31,19 @@ class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
 
     tags = TagSerializer(many=True, required=False)
+    ingredients = IngredientSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
-        fields = ["id", "title", "time_minutes", "price", "link", "tags"]
+        fields = [
+            "id",
+            "title",
+            "time_minutes",
+            "price",
+            "link",
+            "tags",
+            "ingredients",
+        ]
         read_only_fields = ["id"]
 
     def _get_or_create(self, tags, recipe):
@@ -47,11 +56,23 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj)
 
+    def _get_or_create_ingredient(self, ingredients, recipe):
+        """Handle getting or creating ingredients as needed."""
+        auth_user = self.context["request"].user
+        for ingredient in ingredients:
+            ingredient_obj, created = Ingredient.objects.get_or_create(
+                user=auth_user,
+                **ingredient,
+            )
+            recipe.ingredients.add(ingredient_obj)
+
     def create(self, validated_data):
         """Create recipe."""
         tags = validated_data.pop("tags", [])
+        ingredient = validated_data.pop("ingredients", [])
         recipe = Recipe.objects.create(**validated_data)
         self._get_or_create(tags, recipe)
+        self._get_or_create_ingredient(ingredient, recipe)
         return recipe
 
     def update(self, instance, validated_data):
