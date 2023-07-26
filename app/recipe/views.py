f4437da119ca1bbeadcdc4f6fd40f727a78ba218
ww2 programmer <denyssenchuk2005@gmail.com>
2023-07-26 20:08:53 +0300

    Implement tags and ingredients filtering features

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -36,7 +36,7 @@ from recipe import serializers
             OpenApiParameter(
                 "ingredients",
                 OpenApiTypes.STR,
-                description="Comma separated list of ingredients IDs to filter",
+                description="Comma separated list of ingredients IDs to filter",  # noqa
             ),
         ]
     )
@@ -65,7 +65,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
             ingredients_ids = self._params_to_ints(ingredients)
             queryset = queryset.filter(ingredients__id__in=ingredients_ids)
 
-        return queryset.filter(user=self.request.user).order_by("-id").distinct()
+        return (
+            queryset.filter(user=self.request.user).order_by("-id").distinct()
+        )  # noqa
 
     def get_serializer_class(self):
         """Return the serializer class for request."""
@@ -93,6 +95,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "assinged_only",
+                OpenApiTypes.INT,
+                enum=[0, 1],
+                description="Filter by items assigned to recipes",
+            )
+        ]
+    )
+)
 class BaseRecipeAttrViewSet(
     mixins.DestroyModelMixin,
     mixins.UpdateModelMixin,
@@ -104,7 +118,16 @@ class BaseRecipeAttrViewSet(
 
     def get_queryset(self):
         """Filter queryset to authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by("-name")
+        assigned_only = bool(
+            int(self.request.query_params.get("assigned_only", 0))
+        )  # noqa
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return (
+            queryset.filter(user=self.request.user).order_by("-name").distinct()  # noqa
+        )
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
